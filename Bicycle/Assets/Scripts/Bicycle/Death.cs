f891999311a0a23566c7603c0f06094421059454
Jpierrot <68889645+Jpierrot@users.noreply.github.com>
2021-09-08 14:21:20 +0900

    09_08_test

---
+++ Bicycle/Assets/Scripts/Bicycle/Death.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bike;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+
+
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+            Debug.Log("ÆÄ±«µÊ");
+        Destroy(bike, 0.5f);
+    }
+
+
+}
