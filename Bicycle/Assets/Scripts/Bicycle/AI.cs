cb117d8606138fb824966eea6a9b9eef804016fa
Jpierrot <68889645+Jpierrot@users.noreply.github.com>
2021-09-13 23:35:19 +0900

    09_13

--- Bicycle/Assets/Scripts/Bicycle/AI.cs
+++ Bicycle/Assets/Scripts/Bicycle/AI.cs
@@ -7,12 +7,36 @@ public class AI : MonoBehaviour
 {
     public float carSpeed;
     public Transform target;
+    int nextTarget;
 
     // Start is called before the first frame update
     void Start()
     {
+        target = GameManager.instance.target[nextTarget];
+
         GetComponent<NavMeshAgent>().speed = carSpeed;
         GetComponent<NavMeshAgent>().SetDestination(target.position);
+
+        StartCoroutine(AI_Move());
+    }
+
+    IEnumerator AI_Move() {
+        GetComponent<NavMeshAgent>().SetDestination(target.position);
+
+        while(true) {
+            float dis = (target.position - transform.position).magnitude;
+
+            if(dis <= 1) {
+                nextTarget += 1;
+                if(nextTarget >= GameManager.instance.target.Length) {
+                    nextTarget = 0;
+                }
+                target = GameManager.instance.target[nextTarget];
+                GetComponent<NavMeshAgent>().SetDestination(target.position);
+            }
+            yield return null;
+        }
+            
     }
 
     // Update is called once per frame
